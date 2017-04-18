19f7929e4fd67e29b38c4672ec52b09b09133cc3
Michał Cholewiński <cholewinskimichal@gmail.com>
2017-04-18 17:45:37 +0200

    Title Slide

---
+++ presentation/slides/1_titleSlide.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import {
+    Slide
+} from "spectacle";
+
+const Title = () => {
+    return (
+        <Slide>
+            Tests Automation
+        </Slide>
+    );
+}
+
+export default Title;
